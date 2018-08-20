70ff64557d8c1a89afaf34bdf769fae78a84181f
Renée Grinnell <renee.grinnell@gmail.com>
2018-08-19 20:42:35 -0400

    target value functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,39 @@
 //Global variable definitions
-var targetValue = 0;
+var randomValue = 0;
 var yourWins = 0;
 var yourLosses = 0;
 var yourScore = 0;
 
 $(document).ready(function() {
 
-function startGame () {
-//Game start (first time)
-    //Random value between 19 and 120 appears in targetValue
-    //Wins, losses, totalScore all set to = 0
+// //First run of game when user loads page
+function startGame() {
 
-//Crystals 1-4 are each randomly assigned a value between 1 and 12
+    //Generate random value between 19 and 120
+    randomValue = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    console.log(randomValue);
+    
+    // Make random value from above the targetValue for this round
+    $("#targetValue").text(randomValue);
+
+    //Crystals 1-4 are each randomly assigned a value between 1 and 12
 
 }
+
 startGame();
 
-function playGame() {
-//When user clicks a crystal, add its randomly assigned 1-12 value to total-score counter
+// function playGame() {
+// //When user clicks a crystal, add its randomly assigned 1-12 value to total-score counter
 
-//Player WINS if yourScore = target-value, wins go up by 1
+// //Player WINS if yourScore = target-value, wins go up by 1
 
-//Player LOSES if yourScore > target-value, losses go up 1
+// //Player LOSES if yourScore > target-value, losses go up 1
 
-//Game restarts upon win/loss:
-    //Total-score resets to 0
-    //New random target-value generated, displayed
-    //All 4 crystals randomly assigned 4 new hidden values
-}
-playGame();
+// //Game restarts upon win/loss:
+//     //Total-score resets to 0
+//     //New random target-value generated, displayed
+//     //All 4 crystals randomly assigned 4 new hidden values
+// }
+// playGame();
 
 });

ab83e8689444bf2375bfd3985ff3548e788eb131
Renée Grinnell <renee.grinnell@gmail.com>
2018-08-20 19:25:27 -0400

    setup done, now working on adding crystal values to score on click

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,45 +7,62 @@ var yourScore = 0;
 $(document).ready(function() {
 
 // //First run of game when user loads page
-function startGame() {
+function setUpGame() {
 
-    //Generate random value between 19 and 120
+    // Generate random value between 19 and 120
     randomValue = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
     console.log(randomValue);
     
-    // Make random value from above the targetValue for this round
+    // Display the random value from above as the targetValue for this round
     $("#targetValue").text(randomValue);
 
     // Fill array crystalValues with 4 random numbers between 1 and 12
     var crystalValues = [];
     for (var i = 0; i < 4; i++) {
         crystalValues.push(Math.round(Math.random() * 12))
-        console.log(crystalValues);
+        console.log(crystalValues[i]);
     }
 
+    //Working on 4 DISTINCT random values (no repeats)...
+
+    // var crystalValues = [];
+    // while (crystalValues.length < 4) {
+    //     var randomnumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    //     if (crystalValues.indexOf(randomnumber) > -1) continue;
+    //     crystalValues[crystalValues.length] = randomnumber;
+    // }
+    // console.log(arr);
+
+
     // Assign these 4 numbers to respective crystal buttons
-    $('#crystal1').attr("value", crystalValues[0]);
+    $('#crystalButton1').attr("value", crystalValues[0]);
         console.log("Crystal 1 value - test", crystalValues[0]);
-    $('#crystal2').attr("value", crystalValues[1]);
-    $('#crystal3').attr("value", crystalValues[2]);
-    $('#crystal4').attr("value", crystalValues[3]);
+
+    $('#crystalButton2').attr("value", crystalValues[1]);
+        console.log("Crystal 2 value - test", crystalValues[1]);
+
+    $('#crystalButton3').attr("value", crystalValues[2]);
+        console.log("Crystal 3 value - test", crystalValues[2]);
+
+    $('#crystalButton4').attr("value", crystalValues[3]);
+        console.log("Crystal 4 value - test", crystalValues[3]);
 
 }
+setUpGame();
 
-startGame();
+// Game play begins when user clicks their first crystal button
+$(".crystalButton").on("click", playGame);
 
-// function playGame() {
-// //When user clicks a crystal, add its randomly assigned value to total-score counter
+var playGame = function() {
 
-// //Player WINS if yourScore = target-value, wins go up by 1
+    //Player WINS if yourScore = target-value, wins go up by 1
 
-// //Player LOSES if yourScore > target-value, losses go up 1
+    // Player LOSES if yourScore > target-value, losses go up 1
 
-// //Game restarts upon win/loss:
-//     //Total-score resets to 0
-//     //New random target-value generated, displayed
-//     //All 4 crystals randomly assigned 4 new hidden values
-// }
-// playGame();
+    // Game restarts upon win/loss:
+         //Total-score resets to 0
+         //New random target-value generated, displayed
+         //All 4 crystals randomly assigned 4 new hidden values
+}
 
 });
